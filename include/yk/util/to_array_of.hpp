62f5d045094cc9fdc37a7a1ddae68e306f3ae246
KakeyamaY <ykakeyama3014@gmail.com>
2024-08-17 19:24:52 +0900

    add to_array_of (#2)
    * add to_array_of
    
    * allow narrowing conversion
    
    * don't require implicit convertibility

---
+++ include/yk/util/to_array_of.hpp
@@ -0,0 +1,17 @@
+#ifndef YK_UTIL_TO_ARRAY_OF_HPP
+#define YK_UTIL_TO_ARRAY_OF_HPP
+
+#include <array>
+#include <concepts>
+#include <utility>
+
+namespace yk {
+
+template <class T, class... Us>
+constexpr std::array<T, sizeof...(Us)> to_array_of(Us&&... xs) {
+  return std::array<T, sizeof...(Us)>{static_cast<T>(std::forward<Us>(xs))...};
+}
+
+}  // namespace yk
+
+#endif  // YK_UTIL_TO_ARRAY_OF_HPP
