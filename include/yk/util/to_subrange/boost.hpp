d41c44571db142618c6036ef24e5dd0b65085473
KakeyamaY <ykakeyama3014@gmail.com>
2024-08-20 20:36:28 +0900

    add to_subrange (#5)
    * add to_subrange
    
    * use pair
    
    * apply feedback
    
    * modify noexcept specification
    
    * include missing header

---
+++ include/yk/util/to_subrange/boost.hpp
@@ -0,0 +1,17 @@
+#ifndef YK_UTIL_TO_SUBRANGE_BOOST_HPP
+#define YK_UTIL_TO_SUBRANGE_BOOST_HPP
+
+#include <boost/range/iterator_range.hpp>
+#include <ranges>
+
+namespace yk {
+
+template <class Iterator>
+[[nodiscard]] constexpr auto to_subrange(const boost::iterator_range<Iterator>& rng) noexcept(noexcept(std::ranges::subrange(rng.begin(), rng.end())))
+    -> std::ranges::subrange<Iterator> {
+  return std::ranges::subrange(rng.begin(), rng.end());
+}
+
+}  // namespace yk
+
+#endif  // YK_UTIL_TO_SUBRANGE_BOOST_HPP
---
+++ include/yk/util/to_subrange/to_subrange.hpp
@@ -0,0 +1,17 @@
+#ifndef YK_UTIL_TO_SUBRANGE_TO_SUBRANGE_HPP
+#define YK_UTIL_TO_SUBRANGE_TO_SUBRANGE_HPP
+
+#include <ranges>
+#include <utility>
+
+namespace yk {
+
+template <class Iterator>
+[[nodiscard]] constexpr auto to_subrange(const std::pair<Iterator, Iterator>& t) noexcept(noexcept(std::ranges::subrange(t.first, t.second)))
+    -> std::ranges::subrange<Iterator> {
+  return std::ranges::subrange(t.first, t.second);
+}
+
+}  // namespace yk
+
+#endif  // YK_UTIL_TO_SUBRANGE_TO_SUBRANGE_HPP
