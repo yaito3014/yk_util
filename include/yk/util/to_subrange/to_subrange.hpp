e4763ed4bc777cbe30ad3638a5b641ea1cac36db
yaito3014 <ykakeyama3014@gmail.com>
2024-08-20 20:23:12 +0900

    use pair

--- include/yk/util/to_subrange/to_subrange.hpp
+++ include/yk/util/to_subrange/to_subrange.hpp
@@ -2,15 +2,13 @@
 #define YK_UTIL_TO_SUBRANGE_TO_SUBRANGE_HPP
 
 #include <ranges>
-#include <tuple>
+#include <utility>
 
 namespace yk {
 
-template <class IterPair>
-  requires(std::tuple_size_v<std::remove_cvref_t<IterPair>> == 2)
-constexpr auto to_subrange(IterPair&& t) noexcept
-    -> std::ranges::subrange<std::tuple_element_t<0, std::remove_cvref_t<IterPair>>, std::tuple_element_t<1, std::remove_cvref_t<IterPair>>> {
-  return std::ranges::subrange(std::get<0>(std::forward<IterPair>(t)), std::get<1>(std::forward<IterPair>(t)));
+template <class Iterator>
+constexpr auto to_subrange(const std::pair<Iterator, Iterator>& t) noexcept -> std::ranges::subrange<Iterator> {
+  return std::ranges::subrange(t.first, t.second);
 }
 
 }  // namespace yk
