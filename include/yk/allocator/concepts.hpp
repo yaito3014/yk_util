f7e8443ab382d5dfab64570f149ec25dc6e08b56
yaito3014 <ykakeyama3014@gmail.com>
2024-08-20 21:45:44 +0900

    format and add static_assert, constexpr

--- include/yk/allocator/concepts.hpp
+++ include/yk/allocator/concepts.hpp
@@ -4,24 +4,20 @@
 #include <concepts>
 #include <cstddef>
 
-
 namespace yk {
 
-namespace xo { // exposition-only
+namespace xo {  // exposition-only
 
 // C++26
 // https://en.cppreference.com/w/cpp/named_req/Allocator
-template<class Alloc>
-concept simple_allocator =
-    requires(Alloc alloc, std::size_t n) {
-        { *alloc.allocate(n) } -> std::same_as<typename Alloc::value_type&>;
-        { alloc.deallocate(alloc.allocate(n), n) };
-    }
-    && std::copy_constructible<Alloc>
-    && std::equality_comparable<Alloc>;
+template <class Alloc>
+concept simple_allocator = requires(Alloc alloc, std::size_t n) {
+  { *alloc.allocate(n) } -> std::same_as<typename Alloc::value_type&>;
+  { alloc.deallocate(alloc.allocate(n), n) };
+} && std::copy_constructible<Alloc> && std::equality_comparable<Alloc>;
 
-} // xo
+}  // namespace xo
 
-}
+}  // namespace yk
 
 #endif
--- include/yk/allocator/default_init_allocator.hpp
+++ include/yk/allocator/default_init_allocator.hpp
@@ -4,41 +4,38 @@
 #include "yk/allocator/concepts.hpp"
 
 #include <memory>
-#include <utility>
 #include <new>
 #include <type_traits>
-
+#include <utility>
 
 namespace yk {
 
 // http://stackoverflow.com/a/21028912/273767
 
-template<class T, class A = std::allocator<T>>
-class default_init_allocator : public A
-{
-    using traits = std::allocator_traits<A>;
+template <class T, class A = std::allocator<T>>
+class default_init_allocator : public A {
+  static_assert(xo::simple_allocator<A>);
+
+  using traits = std::allocator_traits<A>;
 
 public:
-    template<class U>
-    struct rebind
-    {
-        using other = default_init_allocator<U, typename traits::template rebind_alloc<U>>;
-    };
+  template <class U>
+  struct rebind {
+    using other = default_init_allocator<U, typename traits::template rebind_alloc<U>>;
+  };
 
-    using A::A;
+  using A::A;
 
-    template <typename U>
-    void construct(U* ptr) noexcept(std::is_nothrow_default_constructible_v<U>)
-    {
-        ::new(static_cast<void*>(ptr)) U;
-    }
-    template <typename U, typename...Args>
-    void construct(U* ptr, Args&&... args)
-    {
-        traits::construct(static_cast<A&>(*this), ptr, std::forward<Args>(args)...);
-    }
+  template <class U>
+  constexpr void construct(U* ptr) noexcept(std::is_nothrow_default_constructible_v<U>) {
+    ::new (static_cast<void*>(ptr)) U;
+  }
+  template <class U, class... Args>
+  constexpr void construct(U* ptr, Args&&... args) {
+    traits::construct(static_cast<A&>(*this), ptr, std::forward<Args>(args)...);
+  }
 };
 
-}
+}  // namespace yk
 
 #endif
