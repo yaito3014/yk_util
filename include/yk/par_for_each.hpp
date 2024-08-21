0d5e9058a539094896204a1f1fdd0b6838a66ab9
yaito3014 <ykakeyama3014@gmail.com>
2024-08-21 22:47:06 +0900

    refinement

--- include/yk/par_for_each.hpp
+++ include/yk/par_for_each.hpp
@@ -10,24 +10,38 @@
 #include <execution>
 #include <mutex>
 #include <ranges>
+#include <type_traits>
 #include <utility>
 
 namespace yk {
 
+template <class JobPolicy>
+struct is_job_policy : std::false_type {};
+
+template <class JobPolicy>
+inline constexpr bool is_job_policy_v = is_job_policy<JobPolicy>::value;
+
 namespace execution {
 
 struct abort_policy {};
-
-inline constexpr abort_policy abort{};
-
 struct proceed_policy {};
 
+inline constexpr abort_policy abort{};
 inline constexpr proceed_policy proceed{};
 
 }  // namespace execution
 
-template <class JobPolicy, class Policy, class ForwardIterator, class Func>
+template <>
+struct is_job_policy<execution::abort_policy> : std::true_type {};
+
+template <>
+struct is_job_policy<execution::proceed_policy> : std::true_type {};
+
+template <class JobPolicy, class Policy, std::forward_iterator ForwardIterator, class Func>
 void for_each(JobPolicy&& job_policy, Policy&& policy, ForwardIterator first, ForwardIterator last, Func func) {
+  static_assert(is_job_policy_v<std::remove_cvref_t<JobPolicy>>);
+  static_assert(std::is_execution_policy_v<std::remove_cvref_t<Policy>>);
+  static_assert(std::is_copy_constructible_v<Func>, "std::for_each requires func to be CopyConstructible");
   std::mutex mtx;
   std::exception_ptr exception;
   const auto wrapper = [&, func = std::move(func)]<class T>(T&& arg) noexcept {
@@ -46,7 +60,7 @@ void for_each(JobPolicy&& job_policy, Policy&& policy, ForwardIterator first, Fo
   if (exception) std::rethrow_exception(exception);
 }
 
-template <class Policy, class ForwardIterator, class Func>
+template <class Policy, std::forward_iterator ForwardIterator, class Func>
 void for_each(Policy&& policy, ForwardIterator first, ForwardIterator last, Func func) {
   for_each(execution::abort, std::forward<Policy>(policy), first, last, std::move(func));
 }
@@ -56,7 +70,7 @@ namespace ranges {
 template <class JobPolicy, class Policy, std::ranges::forward_range R, class Func>
 void for_each(JobPolicy&& job_policy, Policy&& policy, R&& r, Func func) {
   ::yk::for_each(std::forward<JobPolicy>(job_policy), std::forward<Policy>(policy), std::ranges::begin(std::forward<R>(r)),
-                 std::ranges::end(std::forward<R>(r)), func);
+                 std::ranges::end(std::forward<R>(r)), std::move(func));
 }
 
 template <class Policy, std::ranges::forward_range R, class Func>
@@ -67,7 +81,7 @@ void for_each(Policy&& policy, R&& r, Func func) {
 template <class JobPolicy, class Policy, std::forward_iterator I, std::sentinel_for<I> S, class Func>
 void for_each(JobPolicy&& job_policy, Policy&& policy, I iter, S sent, Func func) {
   std::ranges::common_view common_view(iter, sent);
-  ::yk::for_each(std::forward<JobPolicy>(job_policy), std::forward<Policy>(policy), common_view.begin(), common_view.end(), func);
+  ::yk::for_each(std::forward<JobPolicy>(job_policy), std::forward<Policy>(policy), common_view.begin(), common_view.end(), std::move(func));
 }
 
 template <class Policy, std::forward_iterator I, std::sentinel_for<I> S, class Func>
