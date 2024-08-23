5957b15cd27dd6d329f32c1c4edfea384208dc4a
yaito3014 <ykakeyama3014@gmail.com>
2024-08-24 03:21:09 +0900

    add nodiscard to to_underlying

--- include/yk/util/to_underlying.hpp
+++ include/yk/util/to_underlying.hpp
@@ -6,7 +6,7 @@
 namespace yk {
 
 template <class T>
-constexpr std::underlying_type_t<T> to_underlying(T value) noexcept {
+[[nodiscard]] constexpr std::underlying_type_t<T> to_underlying(T value) noexcept {
   return static_cast<std::underlying_type_t<T>>(value);
 }
 
