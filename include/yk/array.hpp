481d013054bdb5d60f4af51c4e2a9279b0c54c0e
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2024-08-30 12:04:45 +0900

    Add yk::concat

---
+++ include/yk/array.hpp
@@ -0,0 +1,25 @@
+﻿#ifndef YK_ARRAY_HPP
+#define YK_ARRAY_HPP
+
+#include <algorithm>
+#include <array>
+#include <concepts>
+#include <type_traits>
+
+namespace yk {
+
+template <class T, std::size_t... Sizes>
+  requires std::copy_constructible<T>
+[[nodiscard]] constexpr std::array<T, (Sizes + ...)> concat(const std::array<T, Sizes>&... arrs)  //
+    noexcept(std::is_nothrow_copy_constructible_v<T>) {
+  std::array<T, (Sizes + ...)> res;
+  std::size_t i = 0;
+
+  ((std::copy_n(arrs.begin(), Sizes, res.begin() + i), i += Sizes), ...);
+
+  return res;
+}
+
+}  // namespace yk
+
+#endif  // YK_ARRAY_HPP
