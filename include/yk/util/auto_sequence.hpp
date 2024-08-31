b8d8e55cc47395726f89aa0c30afd9e1165b3eb8
KakeyamaY <ykakeyama3014@gmail.com>
2024-08-31 16:49:51 +0900

    add auto_sequence (#17)

---
+++ include/yk/util/auto_sequence.hpp
@@ -0,0 +1,11 @@
+#ifndef YK_UTIL_AUTO_SEQUENCE_HPP
+#define YK_UTIL_AUTO_SEQUENCE_HPP
+
+namespace yk {
+
+template <auto... Vs>
+struct auto_sequence {};
+
+}  // namespace yk
+
+#endif  // YK_UTIL_AUTO_SEQUENCE_HPP
