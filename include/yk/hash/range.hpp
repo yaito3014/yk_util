e000aae44cf754297b81aa3191a545f20df9e3b3
yaito3014 <ykakeyama3014@gmail.com>
2024-09-06 18:32:50 +0900

    use hash_combine instead of hash_range

--- include/yk/hash/range.hpp
+++ include/yk/hash/range.hpp
@@ -1,19 +1,25 @@
 #ifndef YK_HASH_RANGE_HPP
 #define YK_HASH_RANGE_HPP
 
-#include <boost/container_hash/hash_fwd.hpp>
+#include "yk/hash/hash_combine.hpp"
 
 #include <ranges>
 #include <utility>
 
+#include <cassert>
 #include <cstddef>
 
 namespace yk {
 
+template <class T>
+constexpr std::size_t hash_value_for(const T&) noexcept;
+
 template <std::ranges::range R>
 [[nodiscard]] constexpr std::size_t hash_range(R&& r) noexcept {
-  auto common = std::views::common(std::forward<R>(r));
-  return boost::hash_range(common.begin(), common.end());
+  assert(!std::ranges::empty(r));
+  std::size_t seed = ::yk::hash_value_for(*std::ranges::begin(r));
+  for (auto&& elem : r | std::views::drop(1)) seed = ::yk::hash_combine(seed, elem);
+  return seed;
 }
 
 }  // namespace yk
