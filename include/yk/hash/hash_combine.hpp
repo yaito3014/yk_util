1ae9e2529a6a636d12c8f474430a9bec147a3a1c
yaito3014 <ykakeyama3014@gmail.com>
2024-09-06 19:40:57 +0900

    fix hash_combine related operations

--- include/yk/hash/hash_combine.hpp
+++ include/yk/hash/hash_combine.hpp
@@ -1,18 +1,16 @@
 #ifndef YK_UTIL_HASH_HASH_COMBINE_HPP
 #define YK_UTIL_HASH_HASH_COMBINE_HPP
 
-#include "yk/hash/hash_value_for/fwd.hpp"
-
 #include <boost/container_hash/hash_fwd.hpp>
 
 #include <cstddef>
 
 namespace yk {
 
-template <class T, class... Rest>
-[[nodiscard]] constexpr std::size_t hash_combine(const T& first_arg, const Rest&... rest) noexcept /* strengthened */ {
-  std::size_t seed = ::yk::hash_value_for(first_arg);
-  (boost::hash_combine(seed, ::yk::hash_value_for(rest)), ...);
+template <class... Ts>
+[[nodiscard]] constexpr std::size_t hash_combine(const Ts&... xs) noexcept /* strengthened */ {
+  std::size_t seed = 0;
+  (boost::hash_combine(seed, xs), ...);
   return seed;
 }
 
