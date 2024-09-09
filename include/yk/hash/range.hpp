e2629487b123102eaee953acd044c0c8cb1ce6a4
KakeyamaY <ykakeyama3014@gmail.com>
2024-09-09 16:19:33 +0900

    hash_combine is just a wrapper (#34)
    * hash_combine is just a wrapper
    
    * remove forward declaration header

--- include/yk/hash/range.hpp
+++ include/yk/hash/range.hpp
@@ -1,8 +1,6 @@
 #ifndef YK_HASH_RANGE_HPP
 #define YK_HASH_RANGE_HPP
 
-#include "yk/hash/hash_value_for/fwd.hpp"
-
 #include <boost/container_hash/hash_fwd.hpp>
 
 #include <ranges>
@@ -16,7 +14,7 @@ namespace yk {
 template <std::ranges::range R>
 [[nodiscard]] inline std::size_t hash_range(R&& r) noexcept {
   std::size_t seed = 0;
-  for (auto&& elem : r) boost::hash_combine(seed, ::yk::hash_value_for(elem));
+  for (auto&& elem : r) boost::hash_combine(seed, elem);
   return seed;
 }
 
