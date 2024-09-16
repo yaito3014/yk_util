744fa93d8bb8142435a49904a15341085ad8e311
yaito3014 <ykakeyama3014@gmail.com>
2024-09-16 18:28:08 +0900

    suppress warnings

--- test/test.cpp
+++ test/test.cpp
@@ -97,8 +97,8 @@ struct MyData {
 
   explicit MyData(int value) : value(value), str("IC") {}
 
-  std::string str;
   int value;
+  std::string str;
 };
 
 struct ID {
@@ -324,7 +324,7 @@ BOOST_AUTO_TEST_CASE(ToArrayOf) {
   static_assert(yk::to_array_of<double>(3, 1, 4, 1, 5) == std::array{3.0, 1.0, 4.0, 1.0, 5.0});
 
   enum class StrongID : int {};
-  constexpr auto predefined_ids = yk::to_array_of<StrongID>(1, 2, 3);
+  [[maybe_unused]] constexpr auto predefined_ids = yk::to_array_of<StrongID>(1, 2, 3);
 }
 
 BOOST_AUTO_TEST_CASE(Reverse) {
