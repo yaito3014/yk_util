614afe956798cbc10d8f598fd734f556d60eaf4e
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-04-17 10:54:41 +0900

    Reorganize enum utilities

---
+++ include/yk/enum_bitops.hpp
@@ -0,0 +1,104 @@
+﻿#ifndef YK_ENUM_BITOPS_HPP
+#define YK_ENUM_BITOPS_HPP
+
+#include "yk/util/to_underlying.hpp"
+
+#if !defined(NDEBUG)
+#include <bit>
+#endif
+
+#include <concepts>
+#include <type_traits>
+#include <utility>
+
+#include <cassert>
+#include <cstdint>
+
+namespace yk {
+
+template <class T>
+struct bitops_enabled : std::false_type {};
+
+template <class T>
+constexpr bool bitops_enabled_v = bitops_enabled<T>::value;
+
+namespace detail {
+
+template <class T>
+concept bitops_enum_has_min_bit = std::same_as<std::remove_const_t<decltype(bitops_enabled<T>::min_bit)>, int>;
+
+template <class T>
+concept bitops_enum_has_max_bit = std::same_as<std::remove_const_t<decltype(bitops_enabled<T>::max_bit)>, int>;
+
+}  // namespace detail
+
+template <class T>
+concept BitopsEnabledEnum = std::is_enum_v<T> && bitops_enabled_v<T>;
+
+inline namespace bitops_operators {
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr T operator~(T a) noexcept {
+  return static_cast<T>(~::yk::to_underlying(a));
+}
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr T operator|(T a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return static_cast<T>(::yk::to_underlying(a) | ::yk::to_underlying(b));
+}
+
+template <BitopsEnabledEnum T>
+constexpr T& operator|=(T& a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return a = a | b;
+}
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr T operator&(T a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return static_cast<T>(::yk::to_underlying(a) & ::yk::to_underlying(b));
+}
+
+template <BitopsEnabledEnum T>
+constexpr T& operator&=(T& a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return a = a & b;
+}
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr T operator^(T a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return static_cast<T>(::yk::to_underlying(a) ^ ::yk::to_underlying(b));
+}
+
+template <BitopsEnabledEnum T>
+constexpr T& operator^=(T& a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return a = a ^ b;
+}
+
+}  // namespace bitops_operators
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr bool contains(T a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return (::yk::to_underlying(a) & ::yk::to_underlying(b)) == ::yk::to_underlying(b);
+}
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr bool contains_single_bit(T a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  assert(std::has_single_bit(::yk::to_underlying(b)));
+  return ::yk::to_underlying(a) & ::yk::to_underlying(b);
+}
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr bool contains_any_bit(T a, T b) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  return ::yk::to_underlying(a) & ::yk::to_underlying(b);
+}
+
+}  // namespace yk
+
+#endif  // YK_BITOPS_ENUM_HPP
---
+++ include/yk/enum_bitops_algorithm.hpp
@@ -0,0 +1,32 @@
+#ifndef YK_ENUM_BITOPS_ALGORITHM_HPP
+#define YK_ENUM_BITOPS_ALGORITHM_HPP
+
+#include "yk/util/underlying_type.hpp"
+#include "yk/enum_bitops.hpp"
+
+#include <ranges>
+#include <limits>
+
+namespace yk {
+
+template <BitopsEnabledEnum T>
+[[nodiscard]] constexpr auto each_bit(T flags) noexcept {
+  static_assert(std::is_unsigned_v<std::underlying_type_t<T>>);
+  static_assert(detail::bitops_enum_has_max_bit<T>);
+  static_assert(bitops_enabled<T>::max_bit < std::numeric_limits<std::underlying_type_t<T>>::digits);
+
+  if constexpr (detail::bitops_enum_has_min_bit<T>) {
+    static_assert(bitops_enabled<T>::min_bit <= bitops_enabled<T>::max_bit);
+    return std::views::iota(bitops_enabled<T>::min_bit, bitops_enabled<T>::max_bit + 1)                                         //
+           | std::views::filter([cat = ::yk::to_underlying(flags)](int i) constexpr noexcept -> bool { return (cat >> i) & 1; })  //
+           | std::views::transform([](int i) constexpr noexcept { return static_cast<T>(static_cast<std::underlying_type_t<T>>(1) << i); });
+  } else {
+    return std::views::iota(0, bitops_enabled<T>::max_bit + 1)                                                                   //
+           | std::views::filter([cat = ::yk::to_underlying(flags)](int i) constexpr noexcept -> bool { return (cat >> i) & 1; })  //
+           | std::views::transform([](int i) constexpr noexcept { return static_cast<T>(static_cast<std::underlying_type_t<T>>(1) << i); });
+  }
+}
+
+} // yk
+
+#endif
