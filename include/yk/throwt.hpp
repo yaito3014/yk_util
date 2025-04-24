e8a1e96e0fbb9155e6a1b9f67e53b78907d03a48
yaito3014 <ykakeyama3014@gmail.com>
2025-04-24 19:24:24 +0900

    update

--- include/yk/throwt.hpp
+++ include/yk/throwt.hpp
@@ -11,7 +11,14 @@
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <utility>
+
+#ifndef YK_THROWT_THROW
+
+#define YK_THROWT_THROW(...) throw __VA_ARGS__
+
+#endif
 
 namespace yk {
 
@@ -25,42 +32,52 @@ inline namespace error_functions {
 
 template <class E>
 [[noreturn]] void throwt() {
-  static_assert(std::derived_from<E, std::exception>);
-  static_assert(std::constructible_from<E>);
-  throw boost::enable_error_info(E{}) << detail::traced_type{boost::stacktrace::stacktrace()};
+  static_assert(std::is_base_of_v<std::exception, E>);
+  static_assert(std::is_constructible_v<E>);
+  YK_THROWT_THROW(boost::enable_error_info(E{}) << detail::traced_type{boost::stacktrace::stacktrace()});
 }
 
 template <class E, class Arg, class... Rest>
-  requires std::constructible_from<E, Arg, Rest...> && (!detail::StringLike<Arg>)
+  requires std::is_constructible_v<E, Arg, Rest...> && (!detail::StringLike<Arg>)
 [[noreturn]] void throwt(Arg&& arg, Rest&&... rest) {
-  static_assert(std::derived_from<E, std::exception>);
-  throw boost::enable_error_info(E{std::forward<Arg>(arg), std::forward<Rest>(rest)...})
-      << detail::traced_type{boost::stacktrace::stacktrace()};
+  static_assert(std::is_base_of_v<std::exception, E>);
+  YK_THROWT_THROW(
+      boost::enable_error_info(E{std::forward<Arg>(arg), std::forward<Rest>(rest)...})
+      << detail::traced_type{boost::stacktrace::stacktrace()}
+  );
 }
 
 template <class E, class... Args>
 [[noreturn]] void throwt(std::format_string<Args...> fmt, Args&&... args) {
-  static_assert(std::derived_from<E, std::exception>);
+  static_assert(std::is_base_of_v<std::exception, E>);
   static_assert(std::is_constructible_v<E, std::string>);
-  throw boost::enable_error_info(E{std::format(std::move(fmt), std::forward<Args>(args)...)})
-      << detail::traced_type{boost::stacktrace::stacktrace()};
+  YK_THROWT_THROW(
+      boost::enable_error_info(E{std::format(std::move(fmt), std::forward<Args>(args)...)})
+      << detail::traced_type{boost::stacktrace::stacktrace()}
+  );
 }
 
 template <class E, class Arg0, class... Args>
 [[noreturn]] void throwt(Arg0&& arg0, std::format_string<Args...> fmt, Args&&... args) {
-  static_assert(std::derived_from<E, std::exception>);
+  static_assert(std::is_base_of_v<std::exception, E>);
   static_assert(std::is_constructible_v<E, Arg0, std::string>);
-  throw boost::enable_error_info(E{std::forward<Arg0>(arg0), std::format(std::move(fmt), std::forward<Args>(args)...)})
-      << detail::traced_type{boost::stacktrace::stacktrace()};
+  YK_THROWT_THROW(
+      boost::enable_error_info(E{std::forward<Arg0>(arg0), std::format(std::move(fmt), std::forward<Args>(args)...)})
+      << detail::traced_type{boost::stacktrace::stacktrace()}
+  );
 }
 
 template <class E, class Arg0, class Arg1, class... Args>
 [[noreturn]] void throwt(Arg0&& arg0, Arg1&& arg1, std::format_string<Args...> fmt, Args&&... args) {
-  static_assert(std::derived_from<E, std::exception>);
+  static_assert(std::is_base_of_v<std::exception, E>);
   static_assert(std::is_constructible_v<E, Arg0, Arg1, std::string>);
-  throw boost::enable_error_info(
-      E{std::forward<Arg0>(arg0), std::forward<Arg1>(arg1), std::format(std::move(fmt), std::forward<Args>(args)...)}
-  ) << detail::traced_type{boost::stacktrace::stacktrace()};
+  YK_THROWT_THROW(
+      boost::enable_error_info(
+          E{std::forward<Arg0>(arg0), std::forward<Arg1>(arg1),
+            std::format(std::move(fmt), std::forward<Args>(args)...)}
+      )
+      << detail::traced_type{boost::stacktrace::stacktrace()}
+  );
 }
 
 }  // namespace error_functions
