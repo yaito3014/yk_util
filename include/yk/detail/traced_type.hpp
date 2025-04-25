7a533104afbb22e176da74933a6a3307284f8fad
yaito3014 <ykakeyama3014@gmail.com>
2025-04-25 18:05:23 +0900

    split traced_type into another header

---
+++ include/yk/detail/traced_type.hpp
@@ -0,0 +1,17 @@
+#ifndef YK_DETAIL_TRAICED_TYPE
+#define YK_DETAIL_TRAICED_TYPE
+
+#include <boost/exception/error_info.hpp>
+#include <boost/stacktrace/stacktrace_fwd.hpp>
+
+namespace yk {
+
+namespace detail {
+
+using traced_type = boost::error_info<struct stacktrace_tag, boost::stacktrace::stacktrace>;
+
+}  // namespace detail
+
+}  // namespace yk
+
+#endif  // YK_DETAIL_TRAICED_TYPE
