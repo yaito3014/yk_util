5a7793542291fed434300f942446dff97bcb66bf
Nana Sakisaka <1901813+saki7@users.noreply.github.com>
2025-05-03 22:21:30 +0900

    Fix test

--- test/scheduler.cpp
+++ test/scheduler.cpp
@@ -3,20 +3,18 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <algorithm>
 #include <print>
 #include <format>
+#include <string>
+#include <vector>
 #include <tuple>
 #include <ranges>
+#include <mutex>
 #include <memory>
 #include <type_traits>
 
 namespace {
-
-struct MyData
-{
-  std::string name;
-};
-
 } // anon
 
 BOOST_AUTO_TEST_SUITE(scheduler)
@@ -26,38 +24,47 @@ BOOST_AUTO_TEST_CASE(example)
   auto worker_pool = std::make_shared<yk::exec::worker_pool>();
   worker_pool->set_worker_limit(8);
 
-  const auto ids = std::views::cartesian_product(
-    std::views::iota(0, 10),
-    std::views::iota(0, 10),
-    std::views::iota(0, 10)
-  );
+
+  // emulate std::views::cartesian_product (which does not exist in libc++)
+  constexpr unsigned long long ID_A_MAX = 100;
+  constexpr unsigned long long ID_B_MAX = 100;
+  constexpr unsigned long long ID_C_MAX = 10;
+  constexpr unsigned long long ID_COUNT = ID_A_MAX * ID_B_MAX * ID_C_MAX;
+  using ID = std::tuple<unsigned long long, unsigned long long, unsigned long long>;
+
+  std::mutex mtx;
+  std::vector<ID> results;
+  results.reserve(ID_COUNT);
 
   auto sched = yk::exec::make_scheduler<
     yk::exec::producer_kind::multi_push, yk::exec::consumer_kind::multi_pop,
-    yk::exec::atomic_queue<MyData>
+    yk::exec::atomic_queue<ID>
   >(
     worker_pool,
 
     // Producer
-    [](yk::exec::thread_index_t /*thread_id*/, const std::tuple<int, int, int>& id, auto& queue) {
-        MyData data;
-        data.name = std::format("{}_{}_{}", std::get<0>(id), std::get<1>(id), std::get<2>(id));
-        if (!queue.push_wait(std::move(data))) return;
+    [](yk::exec::thread_index_t /*thread_id*/, unsigned long long id_int, auto& queue) {
+      const auto id_c = id_int % ID_C_MAX;
+      id_int /= ID_C_MAX;
+      const auto id_b = id_int % ID_B_MAX;
+      id_int /= ID_B_MAX;
+      const auto id_a = id_int % ID_A_MAX;
+
+      ID id = std::make_tuple(id_a, id_b, id_c);
+      if (!queue.push_wait(std::move(id))) return;
     },
 
     // Consumer
-    [](yk::exec::thread_index_t /*thread_id*/, auto& queue) {
-        MyData data;
-        if (!queue.pop_wait(data)) return;
-        // ...
-        // ...
-        // do_something(data);
-        // ...
-        // ...
+    [&](yk::exec::thread_index_t /*thread_id*/, auto& queue) {
+      ID id;
+      if (!queue.pop_wait(id)) return;
+
+      std::scoped_lock lock{mtx};
+      results.emplace_back(std::move(id));
     },
 
     // Producer input range (can be any forward_range)
-    ids,
+    std::views::iota(0ull, ID_COUNT),
 
     // queue capacity (= constructor arguments passed to the underlying queue)
     10 * 1024
@@ -69,6 +76,22 @@ BOOST_AUTO_TEST_CASE(example)
   BOOST_REQUIRE_NO_THROW(sched.start());
   BOOST_REQUIRE_NO_THROW(sched.wait_for_all_tasks());
   std::println("simulation done!");
+
+  BOOST_REQUIRE(results.size() == ID_COUNT);
+  std::ranges::sort(results);
+
+  std::vector<ID> expected_results;
+  expected_results.reserve(ID_COUNT);
+
+  for (unsigned long long id_a = 0; id_a < ID_A_MAX; ++id_a) {
+    for (unsigned long long id_b = 0; id_b < ID_B_MAX; ++id_b) {
+      for (unsigned long long id_c = 0; id_c < ID_C_MAX; ++id_c) {
+        expected_results.emplace_back(std::make_tuple(id_a, id_b, id_c));
+      }
+    }
+  }
+
+  BOOST_TEST(std::ranges::equal(results, expected_results) == true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
