c053a6bab56f58db158fcd9fbc0e3091981c29ad
yaito3014 <ykakeyama3014@gmail.com>
2025-05-08 19:33:46 +0900

    extract type_list

---
+++ include/yk/detail/type_list.hpp
@@ -0,0 +1,11 @@
+#ifndef YK_DETAIL_TYPE_LIST_HPP
+#define YK_DETAIL_TYPE_LIST_HPP
+
+namespace yk::detail {
+
+template <class... Ts>
+struct type_list {};
+
+}  // namespace yk::detail
+
+#endif  // YK_DETAIL_TYPE_LIST_HPP
