e824478f77e677d33b1832aabadbe14ac4b4abdb
yaito3014 <ykakeyama3014@gmail.com>
2025-05-08 16:14:15 +0900

    add boolean_testable

---
+++ include/yk/boolean_testable.hpp
@@ -0,0 +1,22 @@
+#ifndef YK_BOOLEAN_TESTABLE_HPP
+#define YK_BOOLEAN_TESTABLE_HPP
+
+#include <concepts>
+
+namespace yk {
+
+namespace detail {
+
+template <class T>
+concept boolean_testable_impl = std::convertible_to<T, bool>;
+
+}  // namespace detail
+
+template <class T>
+concept boolean_testable = detail::boolean_testable_impl<T> && requires(T&& t) {
+  { !std::forward<T>(t) } -> detail::boolean_testable_impl;
+};
+
+}  // namespace yk
+
+#endif  // YK_BOOLEAN_TESTABLE_HPP
