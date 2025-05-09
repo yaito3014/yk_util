780f10d133cc94dd8993f90b8a98aff120640037
yaito3014 <ykakeyama3014@gmail.com>
2025-05-09 17:28:56 +0900

    add compare.hpp

---
+++ include/yk/compare.hpp
@@ -0,0 +1,8 @@
+#ifndef YK_COMPARE_HPP
+#define YK_COMPARE_HPP
+
+#include "yk/compare/comparator.hpp"
+#include "yk/compare/concepts.hpp"
+#include "yk/compare/then.hpp"
+
+#endif  // YK_COMPARE_HPP
