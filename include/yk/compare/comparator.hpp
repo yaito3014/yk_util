e0cb30347d78e19a301c656addc5ae7910bdc440
yaito3014 <ykakeyama3014@gmail.com>
2025-05-09 16:52:47 +0900

    rewrite

--- include/yk/compare/comparator.hpp
+++ include/yk/compare/comparator.hpp
@@ -2,80 +2,181 @@
 #define YK_COMPARE_COMPARATOR
 
 #include "yk/compare/concepts.hpp"
+#include "yk/util/functional.hpp"
 #include "yk/util/specialization_of.hpp"
 #include "yk/no_unique_address.hpp"
 
 #include <compare>
+#include <concepts>
 #include <functional>
+#include <type_traits>
 #include <utility>
 
 namespace yk {
 
-namespace comparators {
+namespace compare {
 
-namespace detail {
+struct comparator_interface {};
 
-template <compare::comparator Comp>
-struct then_closure {
+template <class Comp>
+inline constexpr bool enable_comparator = std::derived_from<Comp, comparator_interface>;
+
+template <class Comp>
+concept comparator = yk::binary_function<Comp> && enable_comparator<Comp>;
+
+struct comparator_adaptor_closure {};
+
+template <class Comp, std::derived_from<comparator_adaptor_closure> Closure>
+constexpr auto operator|(Comp comp, Closure closure) noexcept
+{
+  return std::invoke(std::forward<Closure>(closure), std::move(comp));
+}
+
+template <class Comp>
+struct wrapper_comparator : comparator_interface {
   YK_NO_UNIQUE_ADDRESS Comp comp;
 
-  template <compare::comparator AnotherComp>
-  friend constexpr auto operator|(AnotherComp another, then_closure closure) noexcept
+  constexpr wrapper_comparator(Comp c) noexcept : comp(std::move(c)) {}
+
+  template <class T, class U>
+  constexpr auto operator()(T&& x, U&& y) const noexcept(noexcept(std::is_nothrow_invocable_v<Comp, T, U>))
   {
-    return [=]<class T, class U>(T&& x, U&& y)
-      requires compare::comparator_for<Comp, T, U> && compare::comparator_for<AnotherComp, T, U>
-    {
-      const compare::ordering auto res = std::invoke(another, std::forward<T>(x), std::forward<U>(y));
-      if (res == 0) {
-        return std::invoke(closure.comp, std::forward<T>(x), std::forward<U>(y));
-      } else {
-        return res;
-      }
-    };
+    return std::invoke(comp, std::forward<T>(x), std::forward<U>(y));
   }
 };
 
-struct then_impl {
-  template <compare::comparator Comp>
-  constexpr then_closure<Comp> operator()(Comp comp) const noexcept
+namespace detail {
+
+struct wrap_fn {
+  template <binary_function Comp>
+  constexpr auto operator()(Comp comp) const noexcept
   {
-    return {comp};
+    return wrapper_comparator{std::move(comp)};
   }
 
-  template <compare::comparator Comp1, compare::comparator Comp2>
-  constexpr auto operator()(Comp1 comp1, Comp2 comp2) const noexcept
+  template <comparator Comp>
+  constexpr auto operator()(Comp comp) const noexcept
   {
-    return comp1 | then_closure<Comp2>{comp2};
+    return comp;
   }
 };
 
 }  // namespace detail
 
-inline constexpr detail::then_impl then{};
+namespace comparators {
 
-template <compare::projection F>
-struct extract {
-  YK_NO_UNIQUE_ADDRESS F func;
+inline constexpr detail::wrap_fn wrap{};
+
+}  // namespace comparators
+
+template <class Comp>
+using wrap_t = decltype(comparators::wrap(std::declval<Comp>()));
+
+template <comparator Comp1, comparator Comp2>
+struct then_comparator : comparator_interface {
+  YK_NO_UNIQUE_ADDRESS Comp1 comp1;
+  YK_NO_UNIQUE_ADDRESS Comp2 comp2;
+
+  constexpr then_comparator(Comp1 c1, Comp2 c2) noexcept : comp1(std::move(c1)), comp2(std::move(c2)) {}
 
   template <class T, class U>
-    requires compare::projection_for<F, T> && compare::projection_for<F, U>
+  constexpr auto operator()(T&& x, U&& y) const noexcept(
+      std::is_nothrow_invocable_v<Comp1, T, U> && std::is_nothrow_invocable_v<Comp2, T, U>
+  ) -> std::invoke_result_t<Comp2, T, U>
+  {
+    ordering auto res = std::invoke(comp1, std::forward<T>(x), std::forward<U>(y));
+    if (res == 0) {
+      return std::invoke(comp2, std::forward<T>(x), std::forward<U>(y));
+    } else {
+      return res;
+    }
+  }
+};
+
+template <class Comp1, class Comp2>
+then_comparator(Comp1, Comp2) -> then_comparator<wrap_t<Comp1>, wrap_t<Comp2>>;
+
+namespace detail {
+
+template <class Comp2>
+struct comp_then_closure : comparator_adaptor_closure {
+  YK_NO_UNIQUE_ADDRESS Comp2 comp2;
+
+  constexpr comp_then_closure(Comp2 c2) noexcept : comp2(c2) {}
+
+  template <class Comp1>
+  constexpr auto operator()(Comp1 comp1) const noexcept
+  {
+    return then_comparator{std::move(comp1), std::move(comp2)};
+  }
+};
+
+struct comp_then_fn {
+  template <class Comp1, class Comp2>
+  constexpr auto operator()(Comp1 comp1, Comp2 comp2) const noexcept
+  {
+    return then_comparator{std::move(comp1), std::move(comp2)};
+  }
+
+  template <class Comp2>
+  constexpr auto operator()(Comp2 comp2) const noexcept
+  {
+    return comp_then_closure{std::move(comp2)};
+  }
+};
+
+}  // namespace detail
+
+namespace comparators {
+
+inline constexpr detail::comp_then_fn then{};
+
+}  // namespace comparators
+
+template <unary_function F>
+struct extract_comparator : comparator_interface {
+  YK_NO_UNIQUE_ADDRESS F func;
+
+  constexpr extract_comparator(F f) noexcept : func(std::move(f)) {}
+
+  template <class T, class U>
   constexpr auto operator()(T&& x, U&& y) const noexcept(
       noexcept(std::compare_three_way{}(std::invoke(func, std::forward<T>(x)), std::invoke(func, std::forward<U>(y))))
   )
   {
     return std::compare_three_way{}(std::invoke(func, std::forward<T>(x)), std::invoke(func, std::forward<U>(y)));
   }
+};
 
-  // short-hand syntax
-  template <compare::projection G>
-    requires(!specialization_of<G, detail::then_closure>)
-  friend constexpr auto operator|(extract e, G g) noexcept
+namespace detail {
+
+struct extract_fn {
+  template <class F>
+  constexpr auto operator()(F func) const noexcept
   {
-    return e | then(extract<G>{g});
+    return extract_comparator{func};
   }
 };
 
+}  // namespace detail
+
+namespace comparators {
+
+inline constexpr detail::extract_fn extract{};
+
 }  // namespace comparators
+
+// short-hand syntax
+template <comparator Comp, unary_function F>
+  requires(!std::derived_from<F, comparator_adaptor_closure>)
+constexpr auto operator|(Comp comp, F f) noexcept
+{
+  return comp | comparators::then(comparators::extract(f));
+}
+
+}  // namespace compare
+
+namespace comparators = compare::comparators;
 
 }  // namespace yk
 
