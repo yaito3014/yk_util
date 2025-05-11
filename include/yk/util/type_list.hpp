c23575669d24de6101757fbbeaca3a6f7cb5dce1
yaito3014 <ykakeyama3014@gmail.com>
2025-05-11 22:06:49 +0900

    add size member to type_list

--- include/yk/util/type_list.hpp
+++ include/yk/util/type_list.hpp
@@ -1,10 +1,14 @@
 #ifndef YK_UTIL_TYPE_LIST_HPP
 #define YK_UTIL_TYPE_LIST_HPP
 
+#include <cstdint>
+
 namespace yk {
 
 template <class... Ts>
-struct type_list {};
+struct type_list {
+  static constexpr std::size_t size = sizeof...(Ts);
+};
 
 }  // namespace yk
 
