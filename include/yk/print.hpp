1bd0d354a5dec2076645bf8bf5d0e059f0e87e01
yaito3014 <ykakeyama3014@gmail.com>
2025-05-15 06:08:13 +0900

    split colored_format_string to colorize_(format_)?_string

--- include/yk/print.hpp
+++ include/yk/print.hpp
@@ -5,44 +5,46 @@
 
 #include <print>
 
+#include <cstdio>
+
 #if __cpp_lib_format >= 202311L
 
 namespace yk {
 
 template <class... Args>
-inline void print(std::FILE* stream, colored_format_string<Args...> fmt, Args&&... args)
+inline void print(std::FILE* stream, colorize_format_string<Args...> fmt, Args&&... args)
 {
-  std::print(stream, std::runtime_format(fmt.get()), std::forward<Args>(args)...);
+  std::print(stream, "{}", colorize_and_format(fmt, std::forward<Args>(args)...));
 }
 
 template <class... Args>
-inline void print(std::ostream& os, colored_format_string<Args...> fmt, Args&&... args)
+inline void print(std::ostream& os, colorize_format_string<Args...> fmt, Args&&... args)
 {
-  std::print(os, std::runtime_format(fmt.get()), std::forward<Args>(args)...);
+  std::print(os, "{}", colorize_and_format(fmt, std::forward<Args>(args)...));
 }
 
 template <class... Args>
-inline void print(colored_format_string<Args...> fmt, Args&&... args)
+inline void print(colorize_format_string<Args...> fmt, Args&&... args)
 {
-  std::print(std::runtime_format(fmt.get()), std::forward<Args>(args)...);
+  std::print("{}", colorize_and_format(fmt, std::forward<Args>(args)...));
 }
 
 template <class... Args>
-inline void println(std::FILE* stream, colored_format_string<Args...> fmt, Args&&... args)
+inline void println(std::FILE* stream, colorize_format_string<Args...> fmt, Args&&... args)
 {
-  std::println(stream, std::runtime_format(fmt.get()), std::forward<Args>(args)...);
+  std::println(stream, "{}", colorize_and_format(fmt, std::forward<Args>(args)...));
 }
 
 template <class... Args>
-inline void println(std::ostream& os, colored_format_string<Args...> fmt, Args&&... args)
+inline void println(std::ostream& os, colorize_format_string<Args...> fmt, Args&&... args)
 {
-  std::println(os, std::runtime_format(fmt.get()), std::forward<Args>(args)...);
+  std::println(os, "{}", colorize_and_format(fmt, std::forward<Args>(args)...));
 }
 
 template <class... Args>
-inline void println(colored_format_string<Args...> fmt, Args&&... args)
+inline void println(colorize_format_string<Args...> fmt, Args&&... args)
 {
-  std::println(std::runtime_format(fmt.get()), std::forward<Args>(args)...);
+  std::println("{}", colorize_and_format(fmt, std::forward<Args>(args)...));
 }
 
 }  // namespace yk
--- test/colorize.cpp
+++ test/colorize.cpp
@@ -7,9 +7,9 @@
 
 BOOST_AUTO_TEST_SUITE(print)
 
-BOOST_AUTO_TEST_CASE(colored_format_string)
+BOOST_AUTO_TEST_CASE(colorize_string)
 {
-  constexpr auto test = [](yk::colored_format_string<>) {};
+  constexpr auto test = [](yk::colorize_string) {};
   test("foo");
   test("[black]");
   test("[red]");
@@ -21,6 +21,34 @@ BOOST_AUTO_TEST_CASE(colored_format_string)
   test("[white]");
 }
 
+BOOST_AUTO_TEST_CASE(colorize_format_string)
+{
+  constexpr auto test = [](yk::colorize_format_string<>) {};
+  test("foo");
+  test("[black]");
+  test("[red]");
+  test("[green]");
+  test("[yellow]");
+  test("[blue]");
+  test("[magenta]");
+  test("[cyan]");
+  test("[white]");
+}
+
+BOOST_AUTO_TEST_CASE(colorize)
+{
+  {
+    std::string s;
+    yk::colorize_to(std::back_inserter(s), "[red]foo");
+    BOOST_TEST(s == "\033[0;31mfoo");
+  }
+
+  {
+    auto s = yk::colorize("[green]bar");
+    BOOST_TEST(s == "\033[0;32mbar");
+  }
+}
+
 #if __cpp_lib_format >= 202311L
 
 BOOST_AUTO_TEST_CASE(forward_to_std_print)
@@ -28,6 +56,7 @@ BOOST_AUTO_TEST_CASE(forward_to_std_print)
   std::stringstream ss;
   yk::print(ss, "{}", 42);
   BOOST_TEST(ss.str() == "42");
+  std::format("{}", 42);
 }
 
 #endif
