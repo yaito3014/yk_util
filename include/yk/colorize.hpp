2e367dc7f5e19ac05fcbb063727c5084f3891454
yaito3014 <ykakeyama3014@gmail.com>
2025-05-20 01:27:10 +0900

    reorganize colors

--- include/yk/colorize.hpp
+++ include/yk/colorize.hpp
@@ -74,36 +74,59 @@ namespace detail {
 
 // https://github.com/fmtlib/fmt/blob/master/include/fmt/color.h
 enum class rgb_color : uint32_t {
+  // grayscale
+  white = 0xFFFFFF,        // rgb(255,255,255)
+  white_smoke = 0xF5F5F5,  // rgb(245,245,245)
+  gainsboro = 0xDCDCDC,    // rgb(220,220,220)
+  light_gray = 0xD3D3D3,   // rgb(211,211,211)
+  silver = 0xC0C0C0,       // rgb(192,192,192)
+  dark_gray = 0xA9A9A9,    // rgb(169,169,169)
+  gray = 0x808080,         // rgb(128,128,128)
+  dim_gray = 0x696969,     // rgb(105,105,105)
+  black = 0x000000,        // rgb(0,0,0)
+
+  // pure color
+  red = 0xFF0000,            // rgb(255,0,0)
+  orange_red = 0xFF4500,     // rgb(255,69,0)
+  dark_orange = 0xFF8C00,    // rgb(255,140,0)
+  orange = 0xFFA500,         // rgb(255,165,0)
+  dark_yellow = 0xFFCC00,    // rgb(255,204,0)
+  gold = 0xFFD700,           // rgb(255,215,0)
+  yellow = 0xFFFF00,         // rgb(255,255,0)
+  lime = 0x00FF00,           // rgb(0,255,0)
+  chartreuse = 0x7FFF00,     // rgb(127,255,0)
+  green = 0x008000,          // rgb(0,128,0)
+  spring_green = 0x00FF7F,   // rgb(0,255,127)
+  cyan = 0x00FFFF,           // rgb(0,255,255)
+  deep_sky_blue = 0x00BFFF,  // rgb(0,191,255)
+  blue = 0x0000FF,           // rgb(0,0,255)
+  magenta = 0xFF00FF,        // rgb(255,0,255)
+
+  // others (alphabetical order)
+  // TODO: sort by hue
   alice_blue = 0xF0F8FF,               // rgb(240,248,255)
   antique_white = 0xFAEBD7,            // rgb(250,235,215)
-  aqua = 0x00FFFF,                     // rgb(0,255,255)
   aquamarine = 0x7FFFD4,               // rgb(127,255,212)
   azure = 0xF0FFFF,                    // rgb(240,255,255)
   beige = 0xF5F5DC,                    // rgb(245,245,220)
   bisque = 0xFFE4C4,                   // rgb(255,228,196)
-  black = 0x000000,                    // rgb(0,0,0)
   blanched_almond = 0xFFEBCD,          // rgb(255,235,205)
-  blue = 0x0000FF,                     // rgb(0,0,255)
   blue_violet = 0x8A2BE2,              // rgb(138,43,226)
   brown = 0xA52A2A,                    // rgb(165,42,42)
   burly_wood = 0xDEB887,               // rgb(222,184,135)
   cadet_blue = 0x5F9EA0,               // rgb(95,158,160)
-  chartreuse = 0x7FFF00,               // rgb(127,255,0)
   chocolate = 0xD2691E,                // rgb(210,105,30)
   coral = 0xFF7F50,                    // rgb(255,127,80)
   cornflower_blue = 0x6495ED,          // rgb(100,149,237)
   cornsilk = 0xFFF8DC,                 // rgb(255,248,220)
   crimson = 0xDC143C,                  // rgb(220,20,60)
-  cyan = 0x00FFFF,                     // rgb(0,255,255)
   dark_blue = 0x00008B,                // rgb(0,0,139)
   dark_cyan = 0x008B8B,                // rgb(0,139,139)
   dark_golden_rod = 0xB8860B,          // rgb(184,134,11)
-  dark_gray = 0xA9A9A9,                // rgb(169,169,169)
   dark_green = 0x006400,               // rgb(0,100,0)
   dark_khaki = 0xBDB76B,               // rgb(189,183,107)
   dark_magenta = 0x8B008B,             // rgb(139,0,139)
   dark_olive_green = 0x556B2F,         // rgb(85,107,47)
-  dark_orange = 0xFF8C00,              // rgb(255,140,0)
   dark_orchid = 0x9932CC,              // rgb(153,50,204)
   dark_red = 0x8B0000,                 // rgb(139,0,0)
   dark_salmon = 0xE9967A,              // rgb(233,150,122)
@@ -113,19 +136,12 @@ enum class rgb_color : uint32_t {
   dark_turquoise = 0x00CED1,           // rgb(0,206,209)
   dark_violet = 0x9400D3,              // rgb(148,0,211)
   deep_pink = 0xFF1493,                // rgb(255,20,147)
-  deep_sky_blue = 0x00BFFF,            // rgb(0,191,255)
-  dim_gray = 0x696969,                 // rgb(105,105,105)
   dodger_blue = 0x1E90FF,              // rgb(30,144,255)
   fire_brick = 0xB22222,               // rgb(178,34,34)
   floral_white = 0xFFFAF0,             // rgb(255,250,240)
   forest_green = 0x228B22,             // rgb(34,139,34)
-  fuchsia = 0xFF00FF,                  // rgb(255,0,255)
-  gainsboro = 0xDCDCDC,                // rgb(220,220,220)
   ghost_white = 0xF8F8FF,              // rgb(248,248,255)
-  gold = 0xFFD700,                     // rgb(255,215,0)
   golden_rod = 0xDAA520,               // rgb(218,165,32)
-  gray = 0x808080,                     // rgb(128,128,128)
-  green = 0x008000,                    // rgb(0,128,0)
   green_yellow = 0xADFF2F,             // rgb(173,255,47)
   honey_dew = 0xF0FFF0,                // rgb(240,255,240)
   hot_pink = 0xFF69B4,                 // rgb(255,105,180)
@@ -141,7 +157,6 @@ enum class rgb_color : uint32_t {
   light_coral = 0xF08080,              // rgb(240,128,128)
   light_cyan = 0xE0FFFF,               // rgb(224,255,255)
   light_golden_rod_yellow = 0xFAFAD2,  // rgb(250,250,210)
-  light_gray = 0xD3D3D3,               // rgb(211,211,211)
   light_green = 0x90EE90,              // rgb(144,238,144)
   light_pink = 0xFFB6C1,               // rgb(255,182,193)
   light_salmon = 0xFFA07A,             // rgb(255,160,122)
@@ -150,10 +165,8 @@ enum class rgb_color : uint32_t {
   light_slate_gray = 0x778899,         // rgb(119,136,153)
   light_steel_blue = 0xB0C4DE,         // rgb(176,196,222)
   light_yellow = 0xFFFFE0,             // rgb(255,255,224)
-  lime = 0x00FF00,                     // rgb(0,255,0)
   lime_green = 0x32CD32,               // rgb(50,205,50)
   linen = 0xFAF0E6,                    // rgb(250,240,230)
-  magenta = 0xFF00FF,                  // rgb(255,0,255)
   maroon = 0x800000,                   // rgb(128,0,0)
   medium_aquamarine = 0x66CDAA,        // rgb(102,205,170)
   medium_blue = 0x0000CD,              // rgb(0,0,205)
@@ -173,8 +186,6 @@ enum class rgb_color : uint32_t {
   old_lace = 0xFDF5E6,                 // rgb(253,245,230)
   olive = 0x808000,                    // rgb(128,128,0)
   olive_drab = 0x6B8E23,               // rgb(107,142,35)
-  orange = 0xFFA500,                   // rgb(255,165,0)
-  orange_red = 0xFF4500,               // rgb(255,69,0)
   orchid = 0xDA70D6,                   // rgb(218,112,214)
   pale_golden_rod = 0xEEE8AA,          // rgb(238,232,170)
   pale_green = 0x98FB98,               // rgb(152,251,152)
@@ -188,7 +199,6 @@ enum class rgb_color : uint32_t {
   powder_blue = 0xB0E0E6,              // rgb(176,224,230)
   purple = 0x800080,                   // rgb(128,0,128)
   rebecca_purple = 0x663399,           // rgb(102,51,153)
-  red = 0xFF0000,                      // rgb(255,0,0)
   rosy_brown = 0xBC8F8F,               // rgb(188,143,143)
   royal_blue = 0x4169E1,               // rgb(65,105,225)
   saddle_brown = 0x8B4513,             // rgb(139,69,19)
@@ -197,12 +207,10 @@ enum class rgb_color : uint32_t {
   sea_green = 0x2E8B57,                // rgb(46,139,87)
   sea_shell = 0xFFF5EE,                // rgb(255,245,238)
   sienna = 0xA0522D,                   // rgb(160,82,45)
-  silver = 0xC0C0C0,                   // rgb(192,192,192)
   sky_blue = 0x87CEEB,                 // rgb(135,206,235)
   slate_blue = 0x6A5ACD,               // rgb(106,90,205)
   slate_gray = 0x708090,               // rgb(112,128,144)
   snow = 0xFFFAFA,                     // rgb(255,250,250)
-  spring_green = 0x00FF7F,             // rgb(0,255,127)
   steel_blue = 0x4682B4,               // rgb(70,130,180)
   tan = 0xD2B48C,                      // rgb(210,180,140)
   teal = 0x008080,                     // rgb(0,128,128)
@@ -211,9 +219,6 @@ enum class rgb_color : uint32_t {
   turquoise = 0x40E0D0,                // rgb(64,224,208)
   violet = 0xEE82EE,                   // rgb(238,130,238)
   wheat = 0xF5DEB3,                    // rgb(245,222,179)
-  white = 0xFFFFFF,                    // rgb(255,255,255)
-  white_smoke = 0xF5F5F5,              // rgb(245,245,245)
-  yellow = 0xFFFF00,                   // rgb(255,255,0)
   yellow_green = 0x9ACD32              // rgb(154,205,50)
 };
 
@@ -246,36 +251,55 @@ struct color_pair {
 static constexpr auto color_lookup_table = [] {
   using namespace std::string_view_literals;
   std::array table{
+      color_pair{"white", rgb_color::white},
+      color_pair{"white_smoke", rgb_color::white_smoke},
+      color_pair{"gainsboro", rgb_color::gainsboro},
+      color_pair{"light_gray", rgb_color::light_gray},
+      color_pair{"silver", rgb_color::silver},
+      color_pair{"dark_gray", rgb_color::dark_gray},
+      color_pair{"gray", rgb_color::gray},
+      color_pair{"dim_gray", rgb_color::dim_gray},
+      color_pair{"black", rgb_color::black},
+
+      color_pair{"red", rgb_color::red},
+      color_pair{"orange_red", rgb_color::orange_red},
+      color_pair{"dark_orange", rgb_color::dark_orange},
+      color_pair{"orange", rgb_color::orange},
+      color_pair{"dark_yellow", rgb_color::dark_yellow},
+      color_pair{"gold", rgb_color::gold},
+      color_pair{"yellow", rgb_color::yellow},
+      color_pair{"lime", rgb_color::lime},
+      color_pair{"chartreuse", rgb_color::chartreuse},
+      color_pair{"green", rgb_color::green},
+      color_pair{"spring_green", rgb_color::spring_green},
+      color_pair{"cyan", rgb_color::cyan},
+      color_pair{"deep_sky_blue", rgb_color::deep_sky_blue},
+      color_pair{"blue", rgb_color::blue},
+      color_pair{"magenta", rgb_color::magenta},
+
       color_pair{"alice_blue", rgb_color::alice_blue},
       color_pair{"antique_white", rgb_color::antique_white},
-      color_pair{"aqua", rgb_color::aqua},
       color_pair{"aquamarine", rgb_color::aquamarine},
       color_pair{"azure", rgb_color::azure},
       color_pair{"beige", rgb_color::beige},
       color_pair{"bisque", rgb_color::bisque},
-      color_pair{"black", rgb_color::black},
       color_pair{"blanched_almond", rgb_color::blanched_almond},
-      color_pair{"blue", rgb_color::blue},
       color_pair{"blue_violet", rgb_color::blue_violet},
       color_pair{"brown", rgb_color::brown},
       color_pair{"burly_wood", rgb_color::burly_wood},
       color_pair{"cadet_blue", rgb_color::cadet_blue},
-      color_pair{"chartreuse", rgb_color::chartreuse},
       color_pair{"chocolate", rgb_color::chocolate},
       color_pair{"coral", rgb_color::coral},
       color_pair{"cornflower_blue", rgb_color::cornflower_blue},
       color_pair{"cornsilk", rgb_color::cornsilk},
       color_pair{"crimson", rgb_color::crimson},
-      color_pair{"cyan", rgb_color::cyan},
       color_pair{"dark_blue", rgb_color::dark_blue},
       color_pair{"dark_cyan", rgb_color::dark_cyan},
       color_pair{"dark_golden_rod", rgb_color::dark_golden_rod},
-      color_pair{"dark_gray", rgb_color::dark_gray},
       color_pair{"dark_green", rgb_color::dark_green},
       color_pair{"dark_khaki", rgb_color::dark_khaki},
       color_pair{"dark_magenta", rgb_color::dark_magenta},
       color_pair{"dark_olive_green", rgb_color::dark_olive_green},
-      color_pair{"dark_orange", rgb_color::dark_orange},
       color_pair{"dark_orchid", rgb_color::dark_orchid},
       color_pair{"dark_red", rgb_color::dark_red},
       color_pair{"dark_salmon", rgb_color::dark_salmon},
@@ -285,19 +309,12 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"dark_turquoise", rgb_color::dark_turquoise},
       color_pair{"dark_violet", rgb_color::dark_violet},
       color_pair{"deep_pink", rgb_color::deep_pink},
-      color_pair{"deep_sky_blue", rgb_color::deep_sky_blue},
-      color_pair{"dim_gray", rgb_color::dim_gray},
       color_pair{"dodger_blue", rgb_color::dodger_blue},
       color_pair{"fire_brick", rgb_color::fire_brick},
       color_pair{"floral_white", rgb_color::floral_white},
       color_pair{"forest_green", rgb_color::forest_green},
-      color_pair{"fuchsia", rgb_color::fuchsia},
-      color_pair{"gainsboro", rgb_color::gainsboro},
       color_pair{"ghost_white", rgb_color::ghost_white},
-      color_pair{"gold", rgb_color::gold},
       color_pair{"golden_rod", rgb_color::golden_rod},
-      color_pair{"gray", rgb_color::gray},
-      color_pair{"green", rgb_color::green},
       color_pair{"green_yellow", rgb_color::green_yellow},
       color_pair{"honey_dew", rgb_color::honey_dew},
       color_pair{"hot_pink", rgb_color::hot_pink},
@@ -313,7 +330,6 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"light_coral", rgb_color::light_coral},
       color_pair{"light_cyan", rgb_color::light_cyan},
       color_pair{"light_golden_rod_yellow", rgb_color::light_golden_rod_yellow},
-      color_pair{"light_gray", rgb_color::light_gray},
       color_pair{"light_green", rgb_color::light_green},
       color_pair{"light_pink", rgb_color::light_pink},
       color_pair{"light_salmon", rgb_color::light_salmon},
@@ -322,10 +338,8 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"light_slate_gray", rgb_color::light_slate_gray},
       color_pair{"light_steel_blue", rgb_color::light_steel_blue},
       color_pair{"light_yellow", rgb_color::light_yellow},
-      color_pair{"lime", rgb_color::lime},
       color_pair{"lime_green", rgb_color::lime_green},
       color_pair{"linen", rgb_color::linen},
-      color_pair{"magenta", rgb_color::magenta},
       color_pair{"maroon", rgb_color::maroon},
       color_pair{"medium_aquamarine", rgb_color::medium_aquamarine},
       color_pair{"medium_blue", rgb_color::medium_blue},
@@ -345,8 +359,6 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"old_lace", rgb_color::old_lace},
       color_pair{"olive", rgb_color::olive},
       color_pair{"olive_drab", rgb_color::olive_drab},
-      color_pair{"orange", rgb_color::orange},
-      color_pair{"orange_red", rgb_color::orange_red},
       color_pair{"orchid", rgb_color::orchid},
       color_pair{"pale_golden_rod", rgb_color::pale_golden_rod},
       color_pair{"pale_green", rgb_color::pale_green},
@@ -360,7 +372,6 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"powder_blue", rgb_color::powder_blue},
       color_pair{"purple", rgb_color::purple},
       color_pair{"rebecca_purple", rgb_color::rebecca_purple},
-      color_pair{"red", rgb_color::red},
       color_pair{"rosy_brown", rgb_color::rosy_brown},
       color_pair{"royal_blue", rgb_color::royal_blue},
       color_pair{"saddle_brown", rgb_color::saddle_brown},
@@ -369,12 +380,10 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"sea_green", rgb_color::sea_green},
       color_pair{"sea_shell", rgb_color::sea_shell},
       color_pair{"sienna", rgb_color::sienna},
-      color_pair{"silver", rgb_color::silver},
       color_pair{"sky_blue", rgb_color::sky_blue},
       color_pair{"slate_blue", rgb_color::slate_blue},
       color_pair{"slate_gray", rgb_color::slate_gray},
       color_pair{"snow", rgb_color::snow},
-      color_pair{"spring_green", rgb_color::spring_green},
       color_pair{"steel_blue", rgb_color::steel_blue},
       color_pair{"tan", rgb_color::tan},
       color_pair{"teal", rgb_color::teal},
@@ -383,9 +392,6 @@ static constexpr auto color_lookup_table = [] {
       color_pair{"turquoise", rgb_color::turquoise},
       color_pair{"violet", rgb_color::violet},
       color_pair{"wheat", rgb_color::wheat},
-      color_pair{"white", rgb_color::white},
-      color_pair{"white_smoke", rgb_color::white_smoke},
-      color_pair{"yellow", rgb_color::yellow},
       color_pair{"yellow_green", rgb_color::yellow_green},
   };
   std::ranges::sort(table, {}, &color_pair::name);
