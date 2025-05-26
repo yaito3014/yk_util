5bc5995dd272ff5865487c8731988726648954d1
yaito3014 <ykakeyama3014@gmail.com>
2025-05-26 16:06:31 +0900

    add test

--- test/fixed_string.cpp
+++ test/fixed_string.cpp
@@ -2,11 +2,16 @@
 
 #include <boost/test/unit_test.hpp>
 
+#include <algorithm>
+#include <string_view>
+
 BOOST_AUTO_TEST_SUITE(fixed_string)
 
 BOOST_AUTO_TEST_CASE(fixed_string)
 {
-
+  using namespace std::string_view_literals;
+  constexpr yk::fixed_string str = "foobar";
+  BOOST_TEST((std::ranges::equal(str, "foobar"sv)));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
