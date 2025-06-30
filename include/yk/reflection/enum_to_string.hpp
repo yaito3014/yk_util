73d8718a5e03d9af2a26a61a331dc2f7a3c4f4ec
yaito3014 <ykakeyama3014@gmail.com>
2025-06-30 23:38:52 +0900

    add enum_to_string

---
+++ include/yk/reflection/enum_to_string.hpp
@@ -0,0 +1,28 @@
+#ifndef YK_REFLECTION_ENUM_TO_STRING_HPP
+#define YK_REFLECTION_ENUM_TO_STRING_HPP
+
+#if __cpp_lib_reflection >= 202500L
+
+#include <meta>
+#include <string_view>
+
+namespace yk {
+
+template <class E>
+  requires std::is_enum_v<E>
+constexpr std::string_view enum_to_string(E value)
+{
+  if constexpr (std::meta::is_enumerable_type(^^E))
+    template for (constexpr std::meta::info enumerator : std::define_static_array(std::meta::enumerators_of(^^E))) {
+      if ([:enumerator:] == value) {
+        return std::meta::identifier_of(enumerator);
+      }
+    }
+  return "<unnamed>";
+}
+
+}  // namespace yk
+
+#endif
+
+#endif  // YK_REFLECTION_ENUM_TO_STRING_HPP
